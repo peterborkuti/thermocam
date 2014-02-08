97461a20dd119026cb35c429f23a428a84bd155f
Peter Borkuti <peter@debian>
2014-02-08 22:43:21 +0100

    works with camera

--- src/main.cpp
+++ src/main.cpp
@@ -10,42 +10,38 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video/video.hpp"
 #include "segment.hpp"
 #include "digit.hpp"
 
 #include <iostream>
-#include <fstream>
-#include <ctime>
-#include <stdio.h>
 
 const int NUM_OF_DIGITS = 4;
 
 cv::Mat readImage(int argc, const char** argv, int i) {
 	cv::Mat image;
-	if( argc < 2 || i >= argc )
-	{
-		image = cv::Mat::zeros( 640, 480, CV_8UC3 ); // black image
-	}
-	else {
+	if (argc < 2 || i >= argc) {
+		image = cv::Mat::zeros(640, 480, CV_8UC3); // black image
+	} else {
 		image = cv::imread(argv[i], CV_LOAD_IMAGE_COLOR);   // Read the file
 
-		if(! image.data )                              // Check for invalid input
+		if (!image.data)                              // Check for invalid input
 		{
-			std::cout <<  "Could not open or find the image" << std::endl ;
-			image = cv::Mat( 640, 480, CV_8UC3, cv::Scalar(255,255,255) );
+			std::cout << "Could not open or find the image" << std::endl;
+			image = cv::Mat(640, 480, CV_8UC3, cv::Scalar(255, 255, 255));
 		}
 	}
 
 	return image;
 }
 
-void readData(const cv::Mat image, Digit * d[], Segment& scan, Segment&  hold) {
+void readData(const cv::Mat image, Digit * d[], Segment& scan, Segment& hold) {
 	cv::Mat channels[3];
 	cv::split(image, channels);
 	cv::Mat blue = channels[0];
 	cv::Mat binary;
 	//cv::threshold( blue, binary, threshold_value, max_BINARY_value,threshold_type );
-	cv::threshold( blue, binary, 145, 255, cv::THRESH_BINARY);
+	cv::threshold(blue, binary, 145, 255, cv::THRESH_BINARY);
 
 	for (int i = 0; i < NUM_OF_DIGITS; i++) {
 		d[i]->read(binary);
@@ -74,11 +70,12 @@ void showImage(cv::Mat image, Digit * d[], Segment scan, Segment hold) {
 	imshow("img", image);
 }
 
+const char* keys = { "{1|  | 0 | camera number}" };
 
 int main(int argc, const char** argv) {
 
-	Segment scan(cv::Rect(63,112, 107, 48), "SCAN", "    ");
-	Segment hold(cv::Rect(170,108, 111, 51), "HOLD", "    ");
+	Segment scan(cv::Rect(63, 112, 107, 48), "SCAN", "    ");
+	Segment hold(cv::Rect(170, 108, 111, 51), "HOLD", "    ");
 	scan.setThreshold(0.01);
 	hold.setThreshold(0.01);
 
@@ -89,14 +86,59 @@ int main(int argc, const char** argv) {
 	d[2] = new Digit(cv::Rect(272, 182, 103, 208));
 	d[3] = new Digit(cv::Rect(419, 179, 105, 210));
 
-	for (int im = 1; im < argc; im++) {
-		cv::Mat image = readImage(argc, argv, im);
+	/* From pictures
+	 for (int im = 1; im < argc; im++) {
+	 cv::Mat image = readImage(argc, argv, im);
 
+	 readData(image, d, scan, hold);
+	 printData(d, scan, hold);
+	 showImage(image, d, scan, hold);
+
+	 cv::waitKey(0);
+	 }
+	 */
+	cv::VideoCapture cap;
+
+	cv::CommandLineParser parser(argc, argv, keys);
+	int camNum = parser.get<int>("1");
+
+	cap.open(camNum);
+
+	if (!cap.isOpened()) {
+		//help();
+		std::cout << "***Could not initialize capturing...***\n";
+		std::cout << "Current parameter's value: \n";
+		return (-1);
+	}
+	bool paused = false;
+
+	cv::Mat frame;
+	cv::Mat image;
+	for (;;) {
+		if (!paused) {
+			cap >> frame;
+			if (frame.empty()) {
+				break;
+			}
+		}
+
+		frame.copyTo(image);
 		readData(image, d, scan, hold);
 		printData(d, scan, hold);
 		showImage(image, d, scan, hold);
 
-		cv::waitKey(0);
+		char c = (char) cv::waitKey(10);
+		if (c == 27)
+			break;
+		switch (c) {
+		case 'p':
+			paused = !paused;
+			break;
+		default:
+			;
+		}
 	}
+
+	return 0;
 
 }
