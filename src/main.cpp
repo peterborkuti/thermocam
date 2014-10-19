624fc14d83cc3cffd7bfd147670138cf6bb9e037
Peter Borkuti <peter@debian>
2014-10-19 21:24:59 +0200

    small step

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "cli/cli.hpp"
 #include <cstdio>
 
+
 /*
 int omain() {
 
@@ -88,6 +89,49 @@ int readDistance(dm::DistanceMeter &dm, int times = 0, bool measure_OK = false) 
 	return distance;
 }
 
+int PREV_STATE = -1;
+int STATE = -1;
+int STATE_COUNTER = 0;
+std::string stateStrings[] = {"start", "hold", "scan", "error"};
+
+bool do_dm = false, do_ns = false, do_ir = false;
+
+void initState(int state) {
+	if (state != STATE) {
+		STATE_COUNTER = 1;
+		PREV_STATE = STATE;
+		std::cout << "new state:" << stateStrings[state] << std::endl;
+	}
+	else {
+		STATE_COUNTER++;
+		std::cout << "in state:" << stateStrings[state] << ", "
+			<< STATE_COUNTER <<std::endl;
+	}
+}
+
+void stateStart() {
+	initState(0);
+}
+
+void stateHold() {
+	initState(1);
+}
+
+void stateScan() {
+	initState(2);
+}
+
+void stateError() {
+	initState(3);
+}
+
+void showPictures(cv::Mat const dspImg, cv::Mat camImg) {
+	if (do_ns) cv::imshow("display", dspImg);
+	if (do_ir) cv::imshow("camera", camImg);
+
+	cv::waitKey(3);
+}
+
 int main(int argc, const char** argv) {
 
 	CLI cli;
@@ -97,9 +141,9 @@ int main(int argc, const char** argv) {
 		return 0;
 	}
 
-	bool do_dm = (dm_dev.compare("") != 0);
-	bool do_ir = (ir_cam >= 0);
-	bool do_ns = (ns_cam >= 0);
+	do_dm = (dm_dev.compare("") != 0);
+	do_ir = (ir_cam >= 0);
+	do_ns = (ns_cam >= 0);
 
 	printf("ns_cam: %d, ir_cam:%d\n", ns_cam, ir_cam);
 
@@ -115,7 +159,8 @@ int main(int argc, const char** argv) {
 
 	ns::ScannedValue sv;
 	bool measure_OK = false;
-	int distance = -1;
+	double distance = -1;
+	double storedDistance = -1;
 	std::cout << "dm init - error:" << dm.error << std::endl;
 	if (do_dm) measure_OK = dm.measure(0);
 
@@ -124,13 +169,10 @@ int main(int argc, const char** argv) {
 		if (do_ns) sv = ns.scanCamera();
 		if (do_ir) ir.readCamera();
 
-		if (!do_ns) cv::waitKey(10);
-		if (!do_ir) cv::waitKey(10);
-
 		if (do_dm && measure_OK) {
 			bool success = dm.read();
 			if (success) {
-				distance = dm.getRawDistance();
+				distance = dm.getDistanceInMMeter();
 			}
 			else {
 				std::cout << "dm readDistance - error:" << dm.error << std::endl;
@@ -139,12 +181,38 @@ int main(int argc, const char** argv) {
 
 		if (do_dm) measure_OK = dm.measure(3);
 
-		printf("Distance: %d, Temperature: %03.1f\n", distance, sv.value);
+		printf("Distance: %03.1f mm, Temperature: %03.1f, Scan:%d, Hold:%d\n",
+				distance, sv.value, sv.scan, sv.hold);
 
-		if (do_ns) cv::imshow("display", ns.getBinaryImage());
-		if (do_ir) cv::imshow("camera", ir.getImage());
-		cv::waitKey(30);
+		// Thermometer states
+		// name     | scan | hold | state
+		//----------+------+------+--------
+		// start    | 0    |    0 | Showing the last measured value and laser is on.
+		//                          camera can gets the word in the time of scanning.
+		//                          SHOW PICTURE
+		// hold     | 0    |    1 | Laser is off
+		//                          STORE PICTURE right after state (1,0) with stored distance
+		//                          SHOW PICTURE
+		// scan     | 1    |    0 | Measuring. Showed temperature is false. Laser is ON
+		//                          STORE DISTANCE
+		//                          STORE PICTURE with laser
+		// error    | 1    |    1 | Impossible
 
+		if ((sv.scan == 0) && (sv.hold == 0)) {
+			stateStart();
+		}
+		else if ((sv.scan == 0) && (sv.hold == 1)){
+			stateHold();
+		}
+		else if ((sv.scan == 1) && (sv.hold == 0)){
+			stateScan();
+		}
+		else {
+			stateError();
+		}
+
+
+		showPictures(ns.getBinaryImage(), ir.getImage());
 	}
 
 	std::cout << "END" << std::endl;
